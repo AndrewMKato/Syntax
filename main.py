b7ee2e7db5d13aef7f05a11c6940faa07db79148
Andrew Kato <andrewmkato@gmail.com>
2022-06-12 18:47:47 -0700

    push

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
-    pass
+    with open('nouns.txt', 'r') as f:
+        file = f.readlines()
+
+    nounsFile = [i.rstrip() for i in file]
+    
 
 if __name__ == "__main__":
     main()
